316711adfe5690df6dba42c79aea690268b4a894
Matt Bush <59295261+matthewbush55@users.noreply.github.com>
2021-08-05 20:07:38 -0600

    Added Bootstrap styling

--- README.md
+++ README.md
@@ -1,1 +1,15 @@
-# code-quiz
+# Homework Assignment #4: Code Quiz
+
+## Overview
+
+This assignment demonstrates skills learned so far in the bootcamp by creating a code quiz of JavaScript knowledge. The purpose of this project is to demonstrate my knowledge and understanding of how to put everything we have learned so far into practice. This assignment was completed using jQuery, Bootstrap, HTML, CSS, as well as various components of JavaScript, including functions, arrays, JSON parsing, local storage, and event listeners.
+
+> All code in this assignment was created by me. There was no preset code or boilerplate code to use for this assignment
+
+## Link to Deployed Application
+
+[code-quiz](https://matthewbush55.github.io/code-quiz/)
+
+## Mock-Up
+
+![password-generator](./assets/images/screenshot.png)
--- assets/js/app.js
+++ assets/js/app.js
@@ -1,22 +1,24 @@
 //OBJECT ARRAY FOR QUESTIONS, CHOICES, & ANSWERS
 var questionsObjects = [
   {
-    questionNumber: 1,
-    question: "What color is the sun?",
-    choices: ["A: Green", "B: Black", "C: Blue", "D: Yellow"],
-    answer: "D: Yellow",
+    question: "What is the name of a fast, small, and feature-rich JavaScript library?",
+    choices: ["A: Java", "B: Minify", "C: Coffee", "D: jQuery"],
+    answer: "D: jQuery",
   },
   {
-    questionNumber: 2,
-    question: "What sneaker brand has a swoosh?",
-    choices: ["A: Reebok", "B: Addidas", "C: Under Armor", "D: Nike"],
-    answer: "D: Nike",
+    question: "Which of the following is NOT a JavaScript data type?",
+    choices: ["A: isNaN", "B: String", "C: Boolean", "D: Object"],
+    answer: "A: isNaN",
   },
   {
-    questionNumber: 3,
-    question: "What utencil has ink?",
-    choices: ["A: Pencil", "B: Paper", "C: Fork", "D: Pen"],
-    answer: "D: Pen",
+    question: "What is the === operator called?",
+    choices: ["A: Pipe", "B: Strict Equality", "C: Loose Equality", "D: Equal"],
+    answer: "B: Strict Equality",
+  },
+  {
+    question: "What characters are used to declare an array in JavaScript?",
+    choices: ["A: ()", "B: []", "C: #_#", "D: //"],
+    answer: "B: []",
   },
 ];
 
@@ -30,7 +32,8 @@ var questionWrapper = $(".question-wrapper");
 var submitHighscore = $(".submit-highscore");
 var playerEntry = $("#player");
 var highscoreWrapper = $(".highscore-wrapper");
-var playAgain = $("#play-again");
+var highscoreList = $("#highscore-list");
+var welcomeWrapper = $("#welcome-wrapper");
 
 //GLOBAL VARIABLE DECLARATIONS
 var secondsLeft = 60;
@@ -38,6 +41,7 @@ var questionCount = questionsObjects.length;
 var index = 0;
 var score = 0;
 
+//global page load values for hiding elements that are not yet needed
 submitHighscore.hide();
 highscoreWrapper.hide();
 
@@ -49,6 +53,7 @@ function startTimer() {
     timeEl.text(secondsLeft).prepend("Time: ");
 
     if (secondsLeft <= 0 || index === questionCount) {
+      timeEl.text(0);
       questionWrapper.hide();
       submitHighscore.show();
       clearInterval(timerInterval);
@@ -60,6 +65,7 @@ function startTimer() {
 function startQuiz() {
   submitHighscore.hide();
   highscoreWrapper.hide();
+  welcomeWrapper.hide();
   //call startTimer function
   startTimer();
   //remove start button
@@ -77,6 +83,9 @@ function nextQuestion() {
     var answerLi = $(`<li>`);
     answerLi.addClass("listItemQuestion");
     var answerBtn = $(`<button>`);
+    answerBtn.addClass("btn btn-info btn-lg");
+    answerBtn.attr("style", "margin-top: 5px");
+    answerBtn.attr("style", "margin-bottom: 5px");
     answerBtn.text(questionIndex.choices[i]);
     answerLi.append(answerBtn);
     currentChoices.append(answerLi);
@@ -87,22 +96,19 @@ function nextQuestion() {
 function checkAnswer(event) {
   event.preventDefault();
   var selection = $(event.target).text();
-  console.log(selection);
-  console.log(questionsObjects[index].answer);
   if (selection === questionsObjects[index].answer) {
     score = score + 100;
     index++;
     nextQuestion();
   } else {
-    secondsLeft = secondsLeft - 30;
+    secondsLeft = secondsLeft - 15;
     index++;
     nextQuestion();
   }
 }
 
 //function to display the score and "enter intitials" content when all questions are answered or the timer reaches 0
-function displayScore(event) {
-  event.preventDefault();
+function displayScore() {
   submitHighscore.hide();
   highscoreWrapper.show();
   var highscore = JSON.parse(localStorage.getItem("highscore")) || [];
@@ -112,6 +118,9 @@ function displayScore(event) {
   userObject.score = score;
   highscore.push(userObject);
   localStorage.setItem("highscore", JSON.stringify(highscore));
+  var highScoreLi = $(`<li>`);
+  highScoreLi.text(userInitialsInput + " - " + score);
+  highscoreList.append(highScoreLi);
 }
 
 //event listener when the start button is clicked
@@ -120,5 +129,3 @@ startBtn.on("click", startQuiz);
 questionWrapper.on("click", ".listItemQuestion", checkAnswer);
 //event listener when the submit score button is clicked
 submitBtn.on("click", displayScore);
-//event listener when play again button is clicked
-playAgain.on("click", startQuiz);
